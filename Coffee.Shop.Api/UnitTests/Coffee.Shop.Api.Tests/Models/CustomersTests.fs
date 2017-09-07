0488242e27447d82aaf46915d9a906b51f56e66d
Jon Madden <jmadden@incomm.com>
2017-09-07 13:01:55 -0700

    Finishing test

--- Coffee.Shop.Api/UnitTests/Coffee.Shop.Api.Tests/Models/CustomersTests.fs
+++ Coffee.Shop.Api/UnitTests/Coffee.Shop.Api.Tests/Models/CustomersTests.fs
@@ -9,4 +9,7 @@ module CustomersTests =
         [<Test>]
         member this.ShouldReturnValidCustomer() =
             let result = new Customer(Id = 3, First = "Jon", Last = "Doe")
-            Assert.AreEqual(result.Id, 3)           
+            Assert.AreEqual(result.Id, 3)
+            Assert.AreEqual(result.First, "Jon")
+            Assert.AreEqual(result.Last, "Doe")
+            
